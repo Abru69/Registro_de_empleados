391c2a11c79117a22c5253130e9187bf396b917c
JovanyMolina <jovanymolina822@gmail.com>
2025-10-27 20:54:44 -0600

    feat: add icon

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ if (session_status() === PHP_SESSION_NONE) {
   <link rel="stylesheet" href="public/css/estilos.css">
   <link rel="stylesheet" href="public/css/navbar.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/x-icon" href="public/img/favicon.ico">
 </head>
 <body>
  
